af6c75bb15c1bcde48ae9d3615023e6c5ec243f7
Louis Travaux <louis.trvx@gmail.com>
2023-12-14 19:53:30 +0100

    update (thread): threaded processes and filtered css files from loading. Gained ~30% time

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,16 @@ use headless_chrome::{
             Transport,
         },
     },
+    protocol::cdp::Fetch::{
+        events::RequestPausedEvent,
+        FulfillRequest,
+    },
 };
 use headless_chrome::browser::tab::{
     RequestInterceptor,
     RequestPausedDecision,
 };
+
 use ics::{Event, ICalendar, properties::{
     Summary,
     Location,
@@ -29,11 +34,6 @@ use ics::{Event, ICalendar, properties::{
 use rand::random;
 use anyhow::{Result, anyhow, Context};
 
-
-use headless_chrome::protocol::cdp::{Fetch::events::RequestPausedEvent};
-use headless_chrome::protocol::cdp::Fetch::{ContinueRequest, FailRequest};
-use headless_chrome::protocol::cdp::Network::ErrorReason;
-
 #[derive(Debug, Clone)]
 pub struct PlanningEvent {
     pub start: chrono::naive::NaiveDateTime,
@@ -44,26 +44,29 @@ pub struct PlanningEvent {
     pub notes: String,
 }
 
-struct MyRequestInterceptor;
+struct CssInterceptor;
 
-impl RequestInterceptor for MyRequestInterceptor {
+impl RequestInterceptor for CssInterceptor {
     fn intercept(
         &self,
         _transport: Arc<Transport>,
         _session_id: SessionId,
-        request: RequestPausedEvent,
+        event: RequestPausedEvent,
     ) -> RequestPausedDecision {
-        let url = request.params.request.url;
-        let filetype = request.params.request.headers.0.expect("");
-        print!("URL chargée : {}", url);
-        if !filetype.get("Accept").map_or(false, |s| s.to_string().contains("text/css")) {
-            println!();
+        let filetype = event.params.request.headers.0.expect("No headers");
+        if !filetype.get("Accept").map_or(
+            false,
+            |s| s.to_string().contains("text/css"),
+        ) {
             return RequestPausedDecision::Continue(None);
         }
-        println!("... Blocked!");
-        RequestPausedDecision::Fail(FailRequest {
-            request_id: url,
-            error_reason: ErrorReason::BlockedByClient,
+        RequestPausedDecision::Fulfill(FulfillRequest {
+            request_id: event.params.request_id,
+            response_code: 200,
+            response_headers: None,
+            binary_response_headers: None,
+            body: None,
+            response_phrase: None,
         })
     }
 }
@@ -101,10 +104,10 @@ async fn connect_to_planning(url: &str) -> Result<Vec<PlanningEvent>> {
     let tab = browser.new_tab()?;               // Create a new tab
 
     // Intercept CSS files and refuse them
-    let request_interceptor: Arc<dyn RequestInterceptor + Send + Sync> =
-        Arc::new(MyRequestInterceptor {});
+    let css_request_interceptor: Arc<dyn RequestInterceptor + Send + Sync> =
+        Arc::new(CssInterceptor);
     tab.enable_fetch(None, None)?;
-    tab.enable_request_interception(request_interceptor)?;
+    tab.enable_request_interception(css_request_interceptor.clone())?;
 
     println!("Navigating to {}", url);
     tab.navigate_to(url)?;
@@ -153,7 +156,11 @@ async fn connect_to_planning(url: &str) -> Result<Vec<PlanningEvent>> {
         .parse::<i8>()
         .context("Failed to parse 'NB_WEEKS_TO_SCRAPE'")?;
     for _i in 1..nb_weeks_to_scrape {
+        // create new tabs and enable css interception
         let new_tab = browser.new_tab()?;
+        new_tab.enable_fetch(None, None)?;
+        new_tab.enable_request_interception(css_request_interceptor.clone())?;
+
         tabs.push(new_tab);
     }
 
@@ -163,7 +170,7 @@ async fn connect_to_planning(url: &str) -> Result<Vec<PlanningEvent>> {
         let handle = tokio::spawn(async move {
             let real_week = chrono::Local::now().iso_week().week();
             if (week - real_week) > 0 {
-                println!("New thread for week {} navigating to url", week);
+                println!("New thread for week {} navigating to url", week % 52 + 1);
                 tab.navigate_to(url_clone.as_str())?;
 
                 // wait for div.grilleData to load
@@ -171,6 +178,7 @@ async fn connect_to_planning(url: &str) -> Result<Vec<PlanningEvent>> {
 
                 // click on next week tab
                 tab.find_element(format!("table#x-auto-{}", week - 1).as_str())?
+                    .find_element("button.x-btn-text")?
                     .click()?;
 
                 // wait for div.grilleData to load
@@ -183,113 +191,133 @@ async fn connect_to_planning(url: &str) -> Result<Vec<PlanningEvent>> {
         week += 1;
     }
 
-    println!("Merging events from different threads");
-
     let mut all_events = Vec::new();
     for handle in handles {
-        let events = handle.await??;
-        all_events.extend(events);
+        all_events.extend(handle.await??);
     }
 
     Ok(all_events)
 }
 
-async fn scrape_events(tab: &Arc<Tab>, planning_container: &Vec<i32>, week: &u32) -> Result<Vec<PlanningEvent>> {
+async fn scrape_events(
+    tab: &Arc<Tab>,
+    planning_container: &Vec<i32>,
+    week: &u32,
+) -> Result<Vec<PlanningEvent>> {
     let mut events = Vec::new();
 
-    println!("Scraping week {}", week);
+    println!("Scraping week {}", week % 52 + 1);
 
-    // put all elements in div.grilleData in a vector
-    match tab.find_elements("div.grilleData > div") {
+    // put all elements in div.grilleData in a vector. null vec if no elements
+    match tab.find_elements("div.grilleData > div.event") {
         Ok(elements) => {
             println!("Parsing events for week {}", week);
             // add events of current week to PlanningEvent vec
-            events.extend(parse_events(elements, &week, planning_container).await?);
+            let mut threads = Vec::new();
+            for element in elements {
+                let thread = tokio::spawn(async move {
+                    parse_events(&element, week, planning_container).await
+                });
+
+                threads.push(thread);
+            }
+
+            for thread in threads {
+                events.push(thread.await??);
+            }
+
+            /*events.extend(parse_events(
+                elements,
+                &week,
+                planning_container,
+            ).await?);*/
         }
-        Err(_) => events = Vec::new(),
+        _ => {}
     };
 
     Ok(events)
 }
 
 async fn parse_events(
-    html_events: Vec<Element<'_>>,
+    event: &Element<'_>,
     week: &u32,
     planning_container: &Vec<i32>,
-) -> Result<Vec<PlanningEvent>> {
-    let mut parsed_events = Vec::new();
+) -> Result<PlanningEvent> {
 
-    for event in html_events {
-        let event_position = event.attributes
-            .clone()
-            .context("Failed to get 'div.event' attributes")?
-            .get(1).unwrap()
-            .split("absolute; ")
-            .last()
-            .context("Failed to split 'div.event' style attribute ('absolute; ')")?
-            .split("left: ")
-            .last()
-            .context("Failed to split 'div.event' style attribute ('left: ')")?
-            .split("px; top: ")
-            .map(|s| parse_int(s))
-            .collect::<Vec<_>>();
+    let event_position = event.attributes
+        .clone()
+        .context("Failed to get 'div.event' attributes")?
+        .get(1).unwrap()
+        .split("absolute; ")
+        .last()
+        .context("Failed to split 'div.event' style attribute ('absolute; ')")?
+        .split("left: ")
+        .last()
+        .context("Failed to split 'div.event' style attribute ('left: ')")?
+        .split("px; top: ")
+        .map(|s| parse_int(s))
+        .collect::<Vec<_>>();
 
-        // get event size by selecting table.event child of div.event
-        let event_height = event.find_element("table.event")?
-            .attributes
-            .context("Failed to get 'table.event' attributes")?;
+    // get event size by selecting table.event child of div.event
+    let event_height = event.find_element("table.event")?
+        .attributes
+        .context("Failed to get 'table.event' attributes")?;
 
-        // collect height of table.event (necessary to calculate event duration)
-        let event_height = event_height
-            .get(3)
-            .context("Failed to get 'table.event' style attribute")?
-            .split("height:")
-            .last()
-            .context("Failed to split 'table.event' style attribute ('height:')")?;
+    // collect height of table.event (necessary to calculate event duration)
+    let event_height = event_height
+        .get(3)
+        .context("Failed to get 'table.event' style attribute")?
+        .split("height:")
+        .last()
+        .context("Failed to split 'table.event' style attribute ('height:')")?;
 
-        // get event datas by selecting div.eventText child of div.event
-        let event_datas = event.find_element("div.eventText")?
-            .get_content()
-            .context("Failed to get 'div.eventText' content")?;
-        let mut event_datas = event_datas
-            .split("eventText\">")
-            .last()
-            .context("Failed to split 'div.eventText' content ('eventText\">')")?
-            .split("</b><br>")
-            .collect::<Vec<_>>();
+    // get event datas by selecting div.eventText child of div.event
+    let event_datas = event.find_element("div.eventText")?
+        .get_content()
+        .context("Failed to get 'div.eventText' content")?;
+    let mut event_datas = event_datas
+        .split("eventText\">")
+        .last()
+        .context("Failed to split 'div.eventText' content ('eventText\">')")?
+        .split("</b><br>")
+        .collect::<Vec<_>>();
 
-        // parses the 2nd element of vec into multiple elements
-        event_datas = event_datas.into_iter()
-            .flat_map(|s| s.split("<br>").collect::<Vec<_>>())
-            .collect();
+    // parses the 2nd element of vec into multiple elements
+    event_datas = event_datas.into_iter()
+        .flat_map(|s| s.split("<br>").collect::<Vec<_>>())
+        .collect();
 
-        // remove last element of vec ("</div>")
-        event_datas.pop();
+    // remove last element of vec ("</div>")
+    event_datas.pop();
 
-        // convert event position/width/height to start hour and duration
-        let (start, duration_s) = convert_events(
-            event_position[0],
-            event_position[1],
-            parse_int(event_height),
-            &planning_container,
-            week,
-        ).await?;
+    // convert event position/width/height to start hour and duration
+    let (start, duration_s) = convert_events(
+        event_position[0],
+        event_position[1],
+        parse_int(event_height),
+        &planning_container,
+        week,
+    ).await?;
 
-        // set event datas in struct and push it in parsed_events
-        parsed_events.push(PlanningEvent {
-            start,
-            duration_s,
-            cours: event_datas.remove(0).to_string(),
-            salle: event_datas.remove(0).to_string(),
-            prof: event_datas.remove(0).to_string(),
-            notes: event_datas.pop().unwrap().to_string(),
-        });
-    }
+    Ok(PlanningEvent {
+        start,
+        duration_s,
+        cours: event_datas.remove(0).to_string(),
+        salle: event_datas.remove(0).to_string(),
+        prof: event_datas.remove(0).to_string(),
+        notes: event_datas.pop().unwrap().to_string(),
+    })
 
-    Ok(parsed_events)
 }
 
-async fn convert_events(x: i32, y: i32, height: i32, planning_container: &Vec<i32>, week: &u32) -> Result<(chrono::naive::NaiveDateTime, Duration)> {
+
+async fn convert_events(
+    x: i32,
+    y: i32,
+    height: i32,
+    planning_container: &Vec<i32>,
+    week: &u32,
+) -> Result<(chrono::naive::NaiveDateTime, Duration)> {
 
     // store today 7 am in date format
     let date = chrono::Local::now().date_naive()
@@ -302,7 +330,7 @@ async fn convert_events(x: i32, y: i32, height: i32, planning_container: &Vec<i3
 
     // get px height of half hour and day
     let half_hour_in_px = planning_container[1] as f32 / 28.0; // from 7 to 21, 14 hours -> 28 half hours
-    let day_in_px = planning_container[0] as f32 / 6.0; // 6 days
+    let day_in_px = planning_container[0] as f32 / 7.0; // 7 days
     // calculate days overflow if event is in next week
     let week_overflow = (week - chrono::Local::now().iso_week().week()) * 7;
     // get start date of event (monday 7 am + x days + y half hours)
@@ -326,8 +354,42 @@ async fn create_ics(events: &Vec<PlanningEvent>) -> Result<&str> {
         "https://www.github.com/loouis-t/ut1-timetable",
     );
 
+    let mut handles = Vec::new();
+    for event in events.clone() {
+        println!("new thread for event");
+        let handle = tokio::spawn(async move {
+            // create random uid
+            let uid = format!("{}", random::<i64>());
+
+            // create ics event
+            let mut ics_event = Event::new(
+                uid,
+                Utc::now().format("%Y%m%dT%H%M%SZ").to_string(),
+            );
+            ics_event.push(DtStart::new(
+                event.start.format("%Y%m%dT%H%M%SZ").to_string())
+            );
+            ics_event.push(DtEnd::new(
+                (event.start + event.duration_s)
+                    .format("%Y%m%dT%H%M%SZ").to_string())
+            );
+            ics_event.push(Summary::new(event.cours));
+            ics_event.push(Location::new(event.salle));
+            ics_event.push(Organizer::new(event.prof));
+            ics_event.push(Description::new(event.notes));
+
+            // return event
+            ics_event
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        calendar.add_event(handle.await?);
+    }
+
     // loop over events to create ics events
-    for event in events {
+    /*for event in events {
         // create random uid
         let uid = format!("{}", random::<i64>());
 
@@ -350,7 +412,7 @@ async fn create_ics(events: &Vec<PlanningEvent>) -> Result<&str> {
 
         // Add it to calendar
         calendar.add_event(ics_event);
-    }
+    }*/
 
     // Save ics file in directory
     calendar.save_file("ut1.ics")?;
